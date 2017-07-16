b5cd8114c625523aca5ff151019b73e6b2a53d5c
Richard Chen <richardchen39@gmail.com>
2017-07-16 13:02:55 -0400

    Add csv parser

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,6 +1,9 @@
 from flask import jsonify, render_template, request
 from app import app
 
+from ..models import csv_parser
+
 @app.route('/', methods=['GET'])
 def index():
+  csv_parser.parse_csv()
   return render_template('index.html')
